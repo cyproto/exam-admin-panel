311032476a887219610597ddfd155c5662285f90
cyproto <kyashkarande@gmail.com>
2020-06-09 23:10:27 +0100

    app: routing: Define all the required routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './shared/guard';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: './login/login.module#LoginModule'
+  },
+  {
+    path: 'questions-module',
+    loadChildren: './questions-module/questions-module.module#QuestionsModuleModule',
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: '**', 
+    redirectTo: 'login' 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
