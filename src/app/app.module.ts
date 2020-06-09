1f6bf20631a1219903eb83b0f060a49f5da7d788
cyproto <kyashkarande@gmail.com>
2020-06-09 23:17:59 +0100

    app: module: Import all the required modules, add firebase config.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LayoutModule } from '@angular/cdk/layout';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import * as firebase from 'firebase';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthGuard } from './shared/guard/auth.guard';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEOi6n55MMh52mQEvGZuindi2BD18jsn0",
+  authDomain: "e-assessment-using-blockchain.firebaseapp.com",
+  databaseURL: "https://e-assessment-using-blockchain.firebaseio.com",
+  projectId: "e-assessment-using-blockchain",
+  storageBucket: "e-assessment-using-blockchain.appspot.com",
+  messagingSenderId: "198687170091",
+  appId: "1:198687170091:web:c627e5c8b505301515146e",
+  measurementId: "G-GMVZP94GG0"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      LayoutModule,
+      OverlayModule,
+      HttpClientModule,
+      AngularFireModule,
+      AngularFireDatabaseModule,
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireStorageModule,
   ],
-  providers: [],
+  providers: [AngularFirestore, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
